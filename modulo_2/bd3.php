af36f6c3344db09bb7f5c6b888f39237011116a8
Lizandra Souza <132718769+lizasoarez@users.noreply.github.com>
2023-10-04 08:51:30 -0300

    update

--- modulo_2/bd3.php
+++ modulo_2/bd3.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt">
-    <head>
-    	<meta charset = "utf-8">
-    	<title>Aprendendo PHP</title>
-    </head>
+  <head>
+    <meta charset = "utf-8">
+    <title>Aprendendo PHP</title>
+  </head>
 
-    <body>
+  <body>
 
-          <h2><p>Restaurante Prato Bom </p></h2>
-            <h3><p> Lista de Clientes e tabela - Outra forma de fazer o exercício anterior</p></h3>
+    <h2><p>Restaurante Prato Bom </p></h2>
+    <h3><p> Lista de Clientes e tabela - Outra forma de fazer o exercício anterior</p></h3>
 
-    	<?php
+    	    <?php
 
     	    $host = "localhost";
           $usuario = "usuario";
@@ -32,26 +32,26 @@
          
           foreach($resultados as $cadastro){
 
-             echo "<li>";
-             echo "[";
-             echo $cadastro["id_cliente"];                    
-             echo "]";
-             echo $cadastro["nome"];
-             echo "::";
-             echo $cadastro["cidade"];
-             echo "::";
-             echo $cadastro["pais"];
-             echo "[";
-             echo $cadastro["telefone"];
-             echo "]";
-             echo "</li>";            
-             echo "<br>";                                  
+            echo "<li>";
+            echo "[";
+            echo $cadastro["id_cliente"];                    
+            echo "]";
+            echo $cadastro["nome"];
+            echo "::";
+            echo $cadastro["cidade"];
+            echo "::";
+            echo $cadastro["pais"];
+            echo "[";
+            echo $cadastro["telefone"];
+            echo "]";
+            echo "</li>";            
+            echo "<br>";                                  
              
-            }
+          }
 
-           echo "</ul>";
+            echo "</ul>";
 
-           echo "<table  bgcolor=lightgray border=1 ><tr><td>Id</td><td>Nome</td><td>Cidade</td><td>País</td><td>Telefone</td></tr>";
+            echo "<table  bgcolor=lightgray border=1 ><tr><td>Id</td><td>Nome</td><td>Cidade</td><td>País</td><td>Telefone</td></tr>";
 
            foreach ($resultados as $cadastro){
             $id = $cadastro["id_cliente"];
@@ -60,29 +60,29 @@
             $pais = $cadastro["pais"];
             $telefone = $cadastro["telefone"];
 
-            ?>
+          ?>
 
-            <tr>
-              <td bgcolor=white><?=$id?></td>
-              <td bgcolor=white><?=$nome?></td>
-              <td bgcolor=white><?=$cidade?></td>
-              <td bgcolor=white><?=$pais?></td>
-              <td bgcolor=white><?=$telefone?></td>
-            </tr>
+    <tr>
+     <td bgcolor=white><?=$id?></td>
+     <td bgcolor=white><?=$nome?></td>
+     <td bgcolor=white><?=$cidade?></td>
+     <td bgcolor=white><?=$pais?></td>
+     <td bgcolor=white><?=$telefone?></td>
+    </tr>
 
-           <?php
+          <?php
            }
 
-           echo "</table>";        
+            echo "</table>";        
 
     	    ?>
          
 
-    </body>
+  </body>
     <p><a href="index.php">Menu</a></p> 
 
 
-    </html>
+</html>
     	
 
 
