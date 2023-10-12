fe2d981d6ff4890cd555ec255257f62e8aefb56a
Lizandra Souza <132718769+lizasoarez@users.noreply.github.com>
2023-10-12 17:30:40 -0300

    Módulo 3

--- modulo_3/controledefluxo2.php
+++ modulo_3/controledefluxo2.php
@@ -4,14 +4,15 @@
     <meta charset="utf-8">
     <title>Aprendendo PHP</title>
 </head>
-<body>
+ <body>
 
     <h2><b>Módulo 3</b></h2>
        <h3><b>Aula 2 - Comando "for"</b></h3>
 
-          <?php
+               <?php
+
                $array1 =[10,20,30,40,50,60,70];
-               for($i=0;$i<count($array1);$i++){
+                  for($i=0;$i<count($array1);$i++){             
 
                   echo "No índice [$i] o valor do array array1 é $array1[$i]";
                   echo "<br>";                  
@@ -21,17 +22,14 @@
 
                   echo "<hr>";
                   echo "O primeiro elemento do array1 é: $array1[0]";
-
                   echo "<hr>";
                $ultimo = count($array1) -1;
-                 echo "O último elemento do array1 é: $array1[$ultimo]";
-                 echo "<br>";
+                  echo "O último elemento do array1 é: $array1[$ultimo]";
+                  echo "<br>";
 
-
-          ?>
-
+              ?>
 
     <p><a href="index.php">Menu</a></p>   
 
-</body>
+ </body>
 </html>
